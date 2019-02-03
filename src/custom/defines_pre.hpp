0f426f129679871b74c6ecce4a946e77418a8400
Jason Behik <jason.e.behik@gmail.com>
2019-02-04 02:02:07 +0800

    Tokhangro changes

--- src/custom/defines_pre.hpp
+++ src/custom/defines_pre.hpp
@@ -9,6 +9,12 @@
  * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
  **/
 
+#define PACKETVER 20170614
 
+#ifndef PACKETVER_RE
+#if PACKETVER > 20170614
+#define PACKETVER_RE
+#endif
+#endif
 
 #endif // _CONFIG_CUSTOM_DEFINES_PRE_HPP_
